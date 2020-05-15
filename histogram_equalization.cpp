9a0deccafe59d049b1f219a025abcc8fe9354472
Dudododo <a752601260@gmail.com>
2020-05-15 14:32:53 +0800

    添加项目文件。

---
+++ histogram_equalization.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	cv::Mat srcMat = imread("etest.jpg", 1);
+	cv::Mat resMat;
+
+	cv::Mat src_channels[3];
+	cv::Mat res_channels[3];
+
+	//对图像进行通道分离
+	split(srcMat, src_channels);
+
+	//对每个通道进行直方图均衡
+	for (int i = 0; i <= 2; i++)
+	{
+		equalizeHist(src_channels[i], res_channels[i]);	
+	}
+
+	//对图像进行通道合并
+	merge(res_channels, 3, resMat);
+
+	imshow("B", src_channels[0]);
+	imshow("G", src_channels[1]);
+	imshow("R", src_channels[2]);
+	imshow("resMat", resMat);
+
+	waitKey(0);
+	return 0;
+}
+
